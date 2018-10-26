0f8af1a819b31bdb974fd5bf3b734a37eeb9de48
Qcyan <1165518359@qq.com>
2018-10-26 18:07:16 +0800

    tab切换

--- js/cart.js
+++ js/cart.js
@@ -1,43 +1,44 @@
 app = new Vue({
   el: '#app',
   data: {
-    allSelect:false,
+    allSelect: false,
+    renderList: [],
     list: [
       {
         shopname: '商铺1',
-        id:8,
-        goods:[
+        id: 8,
+        goods: [
           {
-            type:1,
+            type: 1,
             goodsname: '无花果',
             count: 2,
             maxcount: 30,
             price: '18元',
-            id:222,
-            Specifi:{
-              one:[
+            id: 222,
+            Specifi: {
+              one: [
                 {id: "3271", value: "大只", sort: "4"},
                 {id: "3272", value: "小只", sort: "6"}
               ],
-              two:[
+              two: [
                 {id: "3273", value: "褐色", sort: "4"},
                 {id: "3274", value: "白色", sort: "6"}
               ]
             }
           },
           {
-            type:1,
+            type: 1,
             goodsname: '甜果',
             count: 2,
             maxcount: 30,
             price: '18元',
-            id:223,
-            Specifi:{
-              one:[
+            id: 223,
+            Specifi: {
+              one: [
                 {id: "3271", value: "大只", sort: "4"},
                 {id: "3272", value: "小只", sort: "6"}
               ],
-              two:[
+              two: [
                 {id: "3273", value: "褐色", sort: "4"},
                 {id: "3274", value: "白色", sort: "6"}
               ]
@@ -49,39 +50,39 @@ app = new Vue({
       },
       {
         shopname: '商铺2',
-        id:9,
-        goods:[
+        id: 9,
+        goods: [
           {
-            type:2,
+            type: 2,
             goodsname: '咸鱼',
             count: 2,
             maxcount: 30,
             price: '18元',
-            id:224,
-            Specifi:{
-              one:[
+            id: 224,
+            Specifi: {
+              one: [
                 {id: "3271", value: "大只", sort: "4"},
                 {id: "3272", value: "小只", sort: "6"}
               ],
-              two:[
+              two: [
                 {id: "3273", value: "褐色", sort: "4"},
                 {id: "3274", value: "白色", sort: "6"}
               ]
             }
           },
           {
-            type:1,
+            type: 1,
             goodsname: '甜鱼',
             count: 2,
             maxcount: 30,
             price: '18元',
-            id:225,
-            Specifi:{
-              one:[
+            id: 225,
+            Specifi: {
+              one: [
                 {id: "3271", value: "大只", sort: "4"},
                 {id: "3272", value: "小只", sort: "6"}
               ],
-              two:[
+              two: [
                 {id: "3273", value: "褐色", sort: "4"},
                 {id: "3274", value: "白色", sort: "6"}
               ]
@@ -91,73 +92,123 @@ app = new Vue({
 
       }
     ],
-    seltype_num:0,
-    headsel_value:['全部','减价中','折扣']
+    seltype_num: 0,
+    headsel_value: ['全部', '减价中', '折扣'],
+    timer:false
   },
-  computed: {
-  },
+  computed: {},
   methods: {
-    selAll:function(target){
+    selAll: function (target) {
       this.allSelect = target.checked
       var _this = this
-      $.each(this.list,function (index,value) {
+      $.each(this.list, function (index, value) {
         console.log(value)
-          value.sel = _this.allSelect
-        $.each(_this.list[index].goods,function (idx,val) {
+        value.sel = _this.allSelect
+        $.each(_this.list[index].goods, function (idx, val) {
           val.sel = _this.allSelect
         })
       })
     },
-    selshop:function (item) {
+    selshop: function (item) {
       item.sel = !item.sel
-      $.each(item.goods,function (idx,val) {
-        val.sel=item.sel
+      $.each(item.goods, function (idx, val) {
+        val.sel = item.sel
       })
       this.selshop_public()
     },
-    selshop_public:function () {
+    selshop_public: function () {
       // 根据商铺显示判断是否显示总全选
       var selshop_state = true
-      $.each(this.list,function (index,value) {
-        if(!value.sel){
+      $.each(this.list, function (index, value) {
+        if (!value.sel) {
           selshop_state = false
         }
       })
-      if(!selshop_state){
+      if (!selshop_state) {
         this.allSelect = false
-      }else{
+      } else {
         this.allSelect = true
       }
     },
-    selgoods:function (goods,shopIndex) {
+    selgoods: function (goods, shopIndex) {
       goods.sel = !goods.sel
-      var selshop_state = true
-      $.each(this.list[shopIndex].goods,function (idx,val) {
-        if(val.sel==false){
-          selshop_state = false
-        }
-      })
-      if(!selshop_state){
-        this.list[shopIndex].sel = false
-      }else{
-        this.list[shopIndex].sel = true
-      }
-      this.selshop_public()
+
+      clearTimeout(this.timer)
+      this.timer = setTimeout(function () {
+          this.sel_public()
+      },300)
+      // var selshop_state = true
+      // $.each(this.list[shopIndex].goods, function (idx, val) {
+      //   if (val.sel == false) {
+      //     selshop_state = false
+      //   }
+      // })
+      // if (!selshop_state) {
+      //   this.list[shopIndex].sel = false
+      // } else {
+      //   this.list[shopIndex].sel = true
+      // }
+      // this.selshop_public()
     },
-    headsel:function (index) {
-      this.seltype_num = index
+    sel_public:function () {
+      $.each()
+    },
+    headsel: function (index) { //tab切换
+      this.seltype_num = index;
+      //所有勾选状态去掉
+      $.each(this.list, function (index, value) {
+        value.sel = false
+        $.each(value.goods, function (idx, val) {
+          val.sel = false
+        })
+      })
+      //深拷贝，始外部的指向不同，但是push进去的指向还是相同的
+      this.renderList = deepCopy(this.list);
+
+      $.each(this.renderList, function (index, value) {
+        value.goods = []
+      }.bind(this))
+
+      $.each(this.renderList,function (idx,val) {
+        $.each(this.list[idx].goods,function (index,value) {
+            if(value.type == this.seltype_num){
+              val.goods.push(value)
+            }
+          }.bind(this))
+      }.bind(this))
+
+      if(this.seltype_num == 0){
+        this.renderList = this.list
+      }
+
     },
   },
   mounted: function () {
     var _this = this
-    $.each(this.list,function (index,value) {
-      _this.$set(value,'sel',false)
-        $.each(value.goods,function (idx,val) {
-          _this.$set(val,'sel',false)
-        })
+    $.each(this.list, function (index, value) {
+      _this.$set(value, 'sel', false)
+      $.each(value.goods, function (idx, val) {
+        _this.$set(val, 'sel', false)
+      })
     })
-    console.log(this.list)
-  }
+
+
+    this.renderList = this.list;
+  },
 
 })
+
+function deepCopy(obj) {
+  if (!obj || !(obj instanceof Array) && !(obj.toString() === "[object Object]")) return obj;
+  const _obj = obj instanceof Array ? [] : {};
+  for (let key in obj) {
+    if (!obj.hasOwnProperty(key)) continue;
+    if ((obj instanceof Array) || (obj instanceof Object)) {
+      _obj[key] = deepCopy(obj[key]);
+    } else {
+      _obj[key] = obj[key];
+    }
+  }
+  return _obj;
+}
 
