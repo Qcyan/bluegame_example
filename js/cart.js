2589c62765c0d424fd1a63cc39ca67e43c0945c2
Qcyan <1165518359@qq.com>
2018-10-29 18:15:54 +0800

    重新修改规格的数据结构

--- js/cart.js
+++ js/cart.js
@@ -5,7 +5,11 @@ var opts = {
 }
 spec(opts)
 
-
+var opts2 = {
+  template: "#aboutSize",
+  componentName: "about-size"
+}
+spec(opts2)
 
 
 app = new Vue({
@@ -22,40 +26,42 @@ app = new Vue({
           {
             type: 1,
             goodsname: '无花果',
-            count: 2,
-            maxcount: 30,
+            count: 2, //加入购物车的数量
+            // maxcount: 30,
             price: '18元',
             id: 222,
-            kc:8,
-            Specifi: {
-              one: [
-                {id: "3271", value: "大只", sort: "4"},
-                {id: "3272", value: "小只", sort: "6",checked:true}
-              ],
-              two: [
-                {id: "3273", value: "褐色", sort: "4",checked:true},
-                {id: "3274", value: "白色", sort: "6"}
-              ]
-            }
+            kc:8, //当前所选规格的库存
+            sonid:3271,
+            sizeName:'规格：尺码：大只    颜色：红色',
+            Specifi: [
+              {
+                id:'3271',Spec: "111", value: "尺码",name:'大只'
+              },
+              {
+                id:'3271',Spec: "222", value: "颜色",name:'红色'
+              }
+            ],
+            SpecifiId:['111','222']
           },
           {
             type: 1,
             goodsname: '甜果',
             count: 2,
-            maxcount: 30,
+            // maxcount: 30,
             price: '18元',
             id: 223,
             kc:8,
-            Specifi: {
-              one: [
-                {id: "3271", value: "大只", sort: "4",checked:true},
-                {id: "3272", value: "小只", sort: "6"}
-              ],
-              two: [
-                {id: "3273", value: "褐色", sort: "4"},
-                {id: "3274", value: "白色", sort: "6",checked:true}
-              ]
-            }
+            sonid:3272,
+            sizeName:'规格：尺码：大只    颜色：红色',
+            Specifi: [
+              {
+                id:'3272',Spec: "113", value: "尺码",name:'大只'
+              },
+              {
+                id:'3272',Spec: "223", value: "颜色",name:'红色'
+              }
+            ],
+            SpecifiId:['113','223']
           }
         ],
 
@@ -68,40 +74,41 @@ app = new Vue({
           {
             type: 2,
             goodsname: '咸鱼',
-            count: 2,
-            maxcount: 30,
+            count: 2,  //当前加入购物车数量
             price: '18元',
             id: 224,
-            kc:8,
-            Specifi: {
-              one: [
-                {id: "3271", value: "大只", sort: "4",checked:true},
-                {id: "3272", value: "小只", sort: "6"}
-              ],
-              two: [
-                {id: "3273", value: "褐色", sort: "4"},
-                {id: "3274", value: "白色", sort: "6",checked:true}
-              ]
-            }
+            kc:8, //当前规格的库存
+            sonid:3273,
+            sizeName:'规格：尺码：大只    颜色：红色',
+            Specifi: [
+              {
+                id:'3273',Spec: "114", value: "尺码",name:'大只'
+              },
+              {
+                id:'3273',Spec: "224", value: "颜色",name:'红色'
+              }
+            ],
+            SpecifiId:['114','224']
           },
           {
             type: 1,
             goodsname: '甜鱼',
             count: 2,
-            maxcount: 30,
+            // maxcount: 30,
             price: '18元',
             id: 225,
             kc:8,
-            Specifi: {
-              one: [
-                {id: "3271", value: "大只", sort: "4"},
-                {id: "3272", value: "小只", sort: "6",checked:true}
-              ],
-              two: [
-                {id: "3273", value: "褐色", sort: "4",checked:true},
-                {id: "3274", value: "白色", sort: "6"}
-              ]
-            }
+            sonid:3274,
+            sizeName:'规格：尺码：大只    颜色：红色',
+            Specifi: [
+              {
+                id:'3274',Spec: "115", value: "尺码",name:'大只'
+              },
+              {
+                id:'3274',Spec: "225", value: "颜色",name:'红色'
+              }
+            ],
+            SpecifiId:['115','225']
           }
         ]
 
@@ -109,7 +116,32 @@ app = new Vue({
     ],
     seltype_num: 0,
     headsel_value: ['全部', '减价中', '折扣'],
-    timer:false
+    timer:false,
+    clickIndex_size:0,
+    showClick_size:[],
+    showPopUp:false,
+    shopIdx:0,
+    goodsIdx:0,
+    allSpec:[
+      {
+        value:'颜色',
+        id:'3274', //货物的id
+        specifi:[
+          { Spec: "225", value: "颜色",name:'红色',},
+          { Spec: "115", value: "颜色",name:'绿色'},
+          { Spec: "205", value: "颜色",name:'白色'}
+        ]
+      },
+      {
+        value:'尺码',
+        id:'3274',
+        specifi:[
+          { Spec: "225", value: "尺码",name:'大只'},
+          { Spec: "215", value: "尺码",name:'中只'},
+          { Spec: "205", value: "尺码",name:'小只'}
+        ]
+      }
+    ]
   },
   computed: {},
   methods: {
@@ -170,6 +202,64 @@ app = new Vue({
       var goods = compon.goods;
       goods.count = compon.amount;
       console.log(this.list)
+    },
+    //修改规格
+    changeSize:function (e,goodsIdx,shopIdx,itgoods) {
+      var rect = e.currentTarget.closest(".specification")
+      this.shopIdx = shopIdx
+      this.goodsIdx = goodsIdx
+      // console.log(e.currentTarget.closest('.specification').offsetTop)
+      // console.log(e.currentTarget.closest('.specification').offsetLeft)
+      // console.log(e.currentTarget.nextElementSibling.offsetHeight)
+      var sizeHeight = e.currentTarget.nextElementSibling.offsetHeight
+      // var showClick_size = this.list[shopIdx].goods[goodsIdx].Specifi
+      //深拷贝出Specifi，要在这上面操作规格，然后点击确定时给到真正的list上
+      // this.showClick_size = deepCopy(showClick_size)
+      $('#PopUp').css({
+        left:rect.offsetLeft,
+        top:rect.offsetTop + sizeHeight
+      })
+      this.showPopUp = !this.showPopUp
+
+
+      $.each(itgoods.Specifi,function (idxs,vals) {
+        // console.log()
+        $.each(this.allSpec[idxs].specifi,function (indexs,values) {
+          if(vals.Spec == values.Spec){
+            console.log(values)
+            values.checked = true
+          }
+        }.bind(this))
+      }.bind(this))
+      console.log(this.allSpec)
+    },
+    clickSize:function (leixinIdx,detailIdx) {
+      var sizeList = this.showClick_size[leixinIdx].spec
+      $.each(sizeList,function (idx,val) {
+        if(val.checked){
+          val.checked = false
+        }
+        sizeList[detailIdx].checked = true
+      }.bind(this))
+    },
+    comfirm:function () {
+      var theSame = true
+      //拷贝出来的数据跟原始数据对比
+      $.each(this.showClick_size,function (idx,val) {
+        $.each(this.showClick_size[idx].spec,function (index,value) {
+          if(value.checked == true && this.list[this.shopIdx].goods[this.goodsIdx].Specifi[idx].spec[index].checked != true){
+            theSame = false
+          }
+          return
+        }.bind(this))
+      }.bind(this))
+
+      if(theSame){
+        alert('当前修改与之前修改一样')
+      }else{
+        this.list[this.shopIdx].goods[this.goodsIdx].Specifi = this.showClick_size
+      }
+      this.showPopUp = false
     }
   },
   mounted: function () {
